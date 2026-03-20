6dcedca18ea9d95480ba1a81aa45e19e446e6f49
delta student <kumardharmraj703@gmail.com>
2026-03-20 13:48:46 +0530

    fix api url for production

--- admin/src/components/AppointmentsPage/AppointmentsPage.jsx
+++ admin/src/components/AppointmentsPage/AppointmentsPage.jsx
@@ -6,7 +6,7 @@ import { pageStyles, statusClasses, keyframesStyles } from "../../assets/dummySt
 /* ----------------------
   Config
 ------------------------ */
-const API_BASE = "http://localhost:4000";
+const API_BASE = import.meta.env.VITE_API_URL;
 
 /* ----------------------
   Helpers
