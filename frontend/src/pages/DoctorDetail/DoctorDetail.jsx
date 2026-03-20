850e70dce244596c69ee6a8cc6762802e157f9c0
delta student <kumardharmraj703@gmail.com>
2026-03-20 15:16:13 +0530

    routing + vercel issues

--- frontend/src/pages/DoctorDetail/DoctorDetail.jsx
+++ frontend/src/pages/DoctorDetail/DoctorDetail.jsx
@@ -23,7 +23,7 @@ import "react-toastify/dist/ReactToastify.css";
 import { useAuth, useUser } from "@clerk/clerk-react";
 import { doctorDetailStyles } from "../../assets/dummyStyles";
 
-const API_BASE = "http://localhost:4000";
+const API_BASE = import.meta.env.VITE_API_URL;
 
 function getScheduleDates(schedule) {
   if (!schedule) return [];
