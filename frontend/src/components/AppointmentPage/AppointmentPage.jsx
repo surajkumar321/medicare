66c3331d0bc9164b775879bffb100f072e3afb69
delta student <kumardharmraj703@gmail.com>
2026-03-20 14:37:53 +0530

    fix api url production

--- frontend/src/components/AppointmentPage/AppointmentPage.jsx
+++ frontend/src/components/AppointmentPage/AppointmentPage.jsx
@@ -18,7 +18,7 @@ import {
   iconSize,
 } from "../../assets/dummyStyles";
 
-const API_BASE = import.meta.env.VITE_API_URL || "http://localhost:4000";
+const API_BASE = import.meta.env.VITE_API_URL ;
 const API = axios.create({ baseURL: API_BASE });
 
 /* -------------------- Helpers -------------------- */
